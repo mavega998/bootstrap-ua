304eaf2d76a3068266a1ab9dc525110c97fc6b27
Michael Vega <mvega@colegium.com>
2022-01-29 10:09:08 -0500

    Añadiendo nuevas tareas

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('css', ['sass'])
+};
